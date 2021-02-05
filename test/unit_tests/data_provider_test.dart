4f9cd29773f321a283a447a763bc4185151c2cfa
emren <emre.nizamoglu@home-ix.com>
2021-02-05 17:08:53 +0300

    feat: More data provider unit tests

--- test/unit_tests/data_provider_test.dart
+++ test/unit_tests/data_provider_test.dart
@@ -43,5 +43,45 @@ void main() {
       var value = app.timeFrameMenuSelection;
       expect(value, 4);
     });
+
+    test(
+        'getPriceEntryList - '
+        'LineChartData should return when a list of PriceEntry provided', () {
+      var app = DataProvider();
+      var list = <PriceEntry>[PriceEntry(close: 1)];
+      var value = app.getPriceEntryList(list);
+      expect(value.toString(), 'LineChartData');
+    });
+
+    test(
+        'getPercentage - '
+        'Percentage should get when a DataModel '
+        'with a list of PriceEntry provided', () {
+      var app = DataProvider();
+      app.setdataModel(DataModel(the1H: <PriceEntry>[
+        PriceEntry(close: 1),
+        PriceEntry(close: 1),
+        PriceEntry(close: 1)
+      ]));
+      var value = app.getPercentage(1);
+      expect(value.isNotEmpty, true);
+    });
+
+    test('getDataModelSymbol - Data model symbol should get', () {
+      var app = DataProvider();
+      var dtm = DataModel(symbol: 'ISCTR');
+      app.setdataModel(dtm);
+      var value = app.getDataModelSymbol();
+      expect(value, 'ISCTR');
+    });
+
+    test('getLastClosePrice - Data model symbol should get', () {
+      var app = DataProvider();
+      app.setdataModel(DataModel(the1G: <PriceEntry>[
+        PriceEntry(close: 5),
+      ]));
+      var value = app.getLastClosePrice();
+      expect(value, 5.0.toString());
+    });
   });
 }
